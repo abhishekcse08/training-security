03a4ef6fe771f86cdf7b71f7f44ef6757e4318e9
abhishekcse08 <abhishekcse08@gmail.com>
2023-08-16 06:24:20 +0530

    Initial commit

---
+++ src/main/java/com/magnus/secure/TrainingSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.magnus.secure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magnus/secure/TrainingSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magnus.secure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
